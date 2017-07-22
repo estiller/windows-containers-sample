161521598f81819e730480b3dde1fd2260cba493
Eran Stiller <git@stiller.blog>
2017-07-22 18:59:12 +0300

    Add DB to docker-compose file and set appropriate connection strings

--- src/CodeTweet.Web/Startup.cs
+++ src/CodeTweet.Web/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Owin;
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Reflection;
+using Owin;
 
 namespace CodeTweet.Web
 {
@@ -6,7 +9,30 @@ namespace CodeTweet.Web
     {
         public void Configuration(IAppBuilder app)
         {
+            LoadEnvironmentConfig();
             ConfigureAuth(app);
+        }
+
+        private void LoadEnvironmentConfig()
+        {
+            string sqlConnectionString = System.Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            if (sqlConnectionString != null)
+            {
+                var codeTweetConnectionString = ConfigurationManager.ConnectionStrings["CodeTweet"];
+                SetConnectionString(codeTweetConnectionString, sqlConnectionString);
+
+                var identityConnectionString = ConfigurationManager.ConnectionStrings["CodeTweetIdentity"];
+                SetConnectionString(identityConnectionString, sqlConnectionString);
+            }
+        }
+
+        private static void SetConnectionString(ConnectionStringSettings connectionString, string sqlConnectionString)
+        {
+            // This is a hack. Do not use this in production code... :)
+            var fi = typeof(ConfigurationElement).GetField("_bReadOnly", BindingFlags.Instance | BindingFlags.NonPublic);
+            Debug.Assert(fi != null);
+            fi.SetValue(connectionString, false);
+            connectionString.ConnectionString = sqlConnectionString;
         }
     }
 }
