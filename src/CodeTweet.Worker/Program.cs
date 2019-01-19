258fb6510a710dc797f8d3f7134d4a2649c5b550
Eran Stiller <git@stiller.blog>
2019-01-19 14:32:06 +0200

    Add docker support for worker process

--- src/CodeTweet.Worker/Program.cs
+++ src/CodeTweet.Worker/Program.cs
@@ -1,4 +1,7 @@
-﻿using CodeTweet.Notifications;
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Reflection;
+using CodeTweet.Notifications;
 using Topshelf;
 
 namespace CodeTweet.Worker
@@ -7,6 +10,24 @@ namespace CodeTweet.Worker
     {
         static void Main()
         {
+            string rabbitMqConnectionString = System.Environment.GetEnvironmentVariable("RABBITMQ_CONNECTION_STRING");
+            if (rabbitMqConnectionString != null)
+            {
+                ConfigurationManager.AppSettings["RabbitMQ_ConnectionString"] = rabbitMqConnectionString;
+            }
+
+            string sqlConnectionString = System.Environment.GetEnvironmentVariable("CONNECTION_STRING");
+            if (sqlConnectionString != null)
+            {
+                var currentConnectionString = ConfigurationManager.ConnectionStrings["CodeTweetIdentity"];
+
+                // This is a hack. Do not use this in production code... :)
+                var fi = typeof(ConfigurationElement).GetField("_bReadOnly", BindingFlags.Instance | BindingFlags.NonPublic);
+                Debug.Assert(fi != null);
+                fi.SetValue(currentConnectionString, false);
+                currentConnectionString.ConnectionString = sqlConnectionString;
+            }
+
             HostFactory.Run(x =>
             {
                 x.Service<NotificationService>(s =>
